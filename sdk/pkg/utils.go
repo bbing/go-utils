33d39b147aa500d7d6837b1f7e94190f9edc1845
zhangbing <woaiwojia>
2023-03-06 15:09:37 +0800

    response

---
+++ sdk/pkg/utils.go
@@ -0,0 +1,20 @@
+package pkg
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+const (
+	TrafficKey = "X-Request-Id"
+)
+
+// GenerateMsgIDFromContext 生成msgID
+func GenerateMsgIDFromContext(c *gin.Context) string {
+	requestId := c.GetHeader(TrafficKey)
+	if requestId == "" {
+		requestId = uuid.New().String()
+		c.Header(TrafficKey, requestId)
+	}
+	return requestId
+}
