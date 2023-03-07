29318aa54eae33424b41d2a67cb884e8cce0507e
zhangbing <woaiwojia>
2023-03-07 13:58:17 +0800

    ip

--- sdk/pkg/utils.go
+++ sdk/pkg/utils.go
@@ -3,7 +3,6 @@ package pkg
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"strings"
 )
 
 const (
@@ -18,25 +17,4 @@ func GenerateMsgIDFromContext(c *gin.Context) string {
 		c.Header(TrafficKey, requestId)
 	}
 	return requestId
-}
-//获取gin ip
-func GetClientIP(c *gin.Context) string {
-	ClientIP := c.ClientIP()
-	//fmt.Println("ClientIP:", ClientIP)
-	RemoteIP := c.RemoteIP()
-	//fmt.Println("RemoteIP:", RemoteIP)
-	ip := c.Request.Header.Get("X-Forwarded-For")
-	if strings.Contains(ip, "127.0.0.1") || ip == "" {
-		ip = c.Request.Header.Get("X-real-ip")
-	}
-	if ip == "" {
-		ip = "127.0.0.1"
-	}
-	if RemoteIP != "127.0.0.1" {
-		ip = RemoteIP
-	}
-	if ClientIP != "127.0.0.1" {
-		ip = ClientIP
-	}
-	return ip
 }
